f4676321379e330dfa4cae03c4d760b2a57b0a47
Luke <lukeaerickson@gmail.com>
2015-12-24 16:06:29 -0600

    Converting to Groovy

---
+++ src/main/groovy/com/luketim/controller/HomeController.groovy
@@ -0,0 +1,23 @@
+package com.luketim.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/")
+    public String index() {
+        return "index"
+    }
+
+    @RequestMapping(value = "/test")
+    public String test() {
+        return "test"
+    }
+
+    @RequestMapping(value = "/secondTest")
+    public String secondTest() {
+        return "test"
+    }
+}
